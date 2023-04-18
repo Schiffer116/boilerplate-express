48bf614310612bef9570d9c236435d8b346a6b56
Schiffer116 <khanhpg5@gmail.com>
2023-04-18 21:07:12 +0700

    added time

--- myApp.js
+++ myApp.js
@@ -24,6 +24,12 @@ app.get('/json', (req, res) => {
     res.json({ "message": respond });
 });
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ "time": req.time });
+});
 
 
 
